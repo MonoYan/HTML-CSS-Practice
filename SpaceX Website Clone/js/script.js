a0d84234138e1926ffcf2ef29dd18b7bb52dfcd0
Mono Yan <yanshencc@gmail.com>
2023-04-25 16:45:13 +0800

    Hamburger icon & Open animation

---
+++ SpaceX Website Clone/js/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#menu-btn')
+const overlay = document.querySelector('#overlay')
+
+btn.addEventListener('click', navToggle)
+function navToggle(){
+  btn.classList.toggle('open')
+  overlay.classList.toggle('overlay-show')
+  document.body.classList.toggle('stop-scrolling')
+}
