9e0719819999923caf719fe53d76a366a0f777e8
MonoYan <yanshencc@gmail.com>
2023-04-26 15:50:06 +0800

    Inner Page & Fade Anim & Side Menu

--- SpaceX Website Clone/js/script.js
+++ SpaceX Website Clone/js/script.js
@@ -1,9 +1,11 @@
 const btn = document.querySelector('#menu-btn')
 const overlay = document.querySelector('#overlay')
+const menu = document.querySelector('#mobile-menu')
 
 btn.addEventListener('click', navToggle)
 function navToggle(){
   btn.classList.toggle('open')
   overlay.classList.toggle('overlay-show')
   document.body.classList.toggle('stop-scrolling')
+  menu.classList.toggle('show-menu');
 }
