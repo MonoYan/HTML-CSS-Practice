9edf0497488e0bd8a9cd38aaebef045a93143a43
MonoYan <yanshencc@gmail.com>
2023-04-26 16:46:05 +0800

    Remaining Inner Pages

--- SpaceX Website Clone/js/script.js
+++ SpaceX Website Clone/js/script.js
@@ -39,7 +39,7 @@ function countUp(){
         // Round up and set counter value
         counter.innerText = `${Math.ceil(c+increment)}`
 
-        setTimeout(updateCounter, 75)
+        setTimeout(updateCounter, 50)
       }else{
         counter.innerText = target
       }
